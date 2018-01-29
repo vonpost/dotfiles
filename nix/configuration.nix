8d13d079cd01ae1b21bb69154ca7d09bceee552f
dcol <dcol@kth.se>
2018-01-29 19:52:25 +0100

    added shell scripts, emacs daemon, some packages

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -9,6 +9,10 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+  # periodic GC
+  nix.gc.automatic = true;
+  nix.gc.dates = "weekly";
+  nix.gc.options = "--delete-older-than 30d";
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
@@ -19,16 +23,19 @@
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
   boot.extraModulePackages = [ config.boot.kernelPackages.acpi_call config.boot.kernelPackages.tp_smapi ];
-
+  networking.enableIPv6 = false;
   networking.hostName = "LAIN"; # Define your hostname.
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;
   # Select internationalisation properties.
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
   };
-
+  # Shell aliases
+  enviroment.shellAliases = {
+    em = "emacsclient -c"
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
 
@@ -67,6 +74,8 @@
     dunst
     oblogout
     pamixer
+    feh
+    wineStaging
 
     #POWER MANAGEMENT
     powertop
@@ -89,18 +98,20 @@
 
   };
 
-  # set emacs as default editor
+  # EMACS configuration stuff
 
   services.emacs.defaultEditor = true;
+  services.emacs.enable = true;
+  services.emacs.package = with pkgs; (emacsWithPackages (with emacsPackagesNg; [
+      evil
+      haskell-mode
+      intero
+      nix-mode
+      org
+      python-mode
+      frames-only-mode
+  ]));
   
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.bash.enableCompletion = true;
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
@@ -113,7 +124,7 @@
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
-  # Enable the X11 windowing system.
+  # Enable the X11 and xmonad.
   services.xserver.enable = true;
   services.xserver.layout = "us";
   services.xserver.xkbOptions = "eurosign:e";
@@ -127,12 +138,10 @@
 	];
   };
   services.xserver.windowManager.default = "xmonad";
+
   # Enable touchpad support.
   services.xserver.libinput.enable = true;
   services.xserver.displayManager.slim.enable = true;
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.dcol = {
@@ -140,7 +149,7 @@
     createHome = true;
     home = "/home/dcol/";
     description = "Daniel Collin";
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "wheel" "networkmanager"];
     useDefaultShell = true;
   };
   security.sudo.enable = true;
