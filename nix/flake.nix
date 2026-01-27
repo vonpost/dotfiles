8a196aafa48882f08209e8429ccb6c230e90fa16
dcol <dcol@kth.se>
2026-01-27 19:59:59 +0100

    added new vm to flake.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -15,6 +15,7 @@
           KAIZOKU.url = ./vm/KAIZOKU;
           DARE.url = ./vm/DARE;
           OKAMI.url = ./vm/OKAMI;
+          MAMORU.url = ./vm/MAMORU;
         };
 
   outputs =
@@ -68,12 +69,13 @@
           {
             networking.hostName = "MOTHER";
             microvm.stateDir = "/aleph/vm-pool/microvm";
-            microvm.autostart = ["UCHI" "SOTO" "DARE" "OKAMI" "KAIZOKU"];
+            microvm.autostart = ["UCHI" "SOTO" "DARE" "OKAMI" "KAIZOKU" "MAMORU"];
             microvm.vms.UCHI = { flake = UCHI; updateFlake = "git+file:///root/dotfiles/?dir=nix/vm/UCHI"; };
             microvm.vms.SOTO = { flake = SOTO; updateFlake = "git+file:///root/dotfiles/?dir=nix/vm/SOTO"; };
             microvm.vms.DARE = { flake = DARE; updateFlake = "git+file:///root/dotfiles/?dir=nix/vm/DARE"; };
             microvm.vms.KAIZOKU = { flake = KAIZOKU; updateFlake = "git+file:///root/dotfiles/?dir=nix/vm/KAIZOKU"; };
             microvm.vms.OKAMI = { flake = OKAMI; updateFlake = "git+file:///root/dotfiles/?dir=nix/vm/OKAMI"; };
+            microvm.vms.MAMORU = { flake = MAMORU; updateFlake = "git+file:///root/dotfiles/?dir=nix/vm/MAMORU"; };
           }
         ];
       };
